09baeada3a42a345e2aabc86196f529089296745
Anh Tran <anhminhtran268@gmail.com>
2025-11-30 23:13:09 -0500

    built API, configured program.cs, configured connection string

--- TaskManagement/TaskManagement.Core/obj/Debug/net9.0/TaskManagement.Core.AssemblyInfo.cs
+++ TaskManagement/TaskManagement.Core/obj/Debug/net9.0/TaskManagement.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6258630aa55705ca449c5a867934168822a2ccdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c11e67490c9209acfe8c4581b9e7a3139284db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TaskManagement/TaskManagement.Infrastructure/obj/Debug/net9.0/TaskManagement.Infrastructure.AssemblyInfo.cs
+++ TaskManagement/TaskManagement.Infrastructure/obj/Debug/net9.0/TaskManagement.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6258630aa55705ca449c5a867934168822a2ccdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c11e67490c9209acfe8c4581b9e7a3139284db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
