23f6a60d605b3538f68795df2ae88c1b5fabdec4
Anh Tran <anhminhtran268@gmail.com>
2025-12-02 16:46:44 -0500

    Created controller tests

--- TaskManagement/TaskManagement.Tests/obj/Debug/net9.0/TaskManagement.Tests.AssemblyInfo.cs
+++ TaskManagement/TaskManagement.Tests/obj/Debug/net9.0/TaskManagement.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9bc4d5fc2f51e4e6c83d520d56b6196e8577dd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c7ba3cbb3759bf9e2a43baec311ac6c274459b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
